1b44ca72e869623c86692e0a8ec4a0e693930439
Sophie-Ndobe <ndobes76@gmail.com>
2023-09-17 16:03:32 +0200

    Created a form in the SearchCity.js component

--- src/SearchCity.js
+++ src/SearchCity.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./SearchCity.css";
 
 export default function SearchCity() {
-  return <div className="SearchCity">The Search Engine goes here</div>;
+  let [city, setCity] = useState(" ");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`The city is ${city}`);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="SearchCity">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="Search"
+          placeholder="Enter a city name..."
+          onChange={updateCity}
+        ></input>
+        <button className="Search">Search</button>
+        <button>Current</button>
+      </form>
+    </div>
+  );
 }
